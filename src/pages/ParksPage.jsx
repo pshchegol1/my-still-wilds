ec3693cecb15e3433b534985eb99d8c0bb0797cc
Pavlo Shchegol <46579720+pshchegol1@users.noreply.github.com>
2026-09-19 21:34:52 -0600

    Nudge hero content up slightly more

--- src/pages/ParksPage.jsx
+++ src/pages/ParksPage.jsx
@@ -47,7 +47,7 @@ export default function ParksPage() {
             Back
           </button>
 
-          <div className="space-y-6 mb-72">
+          <div className="space-y-6 mb-80">
             <div className="space-y-3 max-w-3xl">
               <h1 className="tracking-wide text-white text-[44px] leading-[1.02] md:text-[64px]">
                 CANADA'S <span className="text-[#38A169]">NATIONAL PARKS</span>
